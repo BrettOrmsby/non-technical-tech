57205bf8ebe198a89760a0b596bbef66598db20d
BrettOrmsby <brettwormsby@outlook.com>
2022-11-09 18:22:25 -0500

    Feat: Added 404 page

---
+++ error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1>404</h1>
+    <p>Page not found</p>
+    <NuxtLink to="/"><Button label="Home" /></NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 9em;
+  line-height: 0;
+}
+.container {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
